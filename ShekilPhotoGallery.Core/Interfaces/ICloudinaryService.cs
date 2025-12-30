a35cde337b81901969bd2c38e84bac9b7f6e2bba
Yusuf YILDIZ <yusufyildiz@ogrenci.beykoz.edu.tr>
2025-12-30 16:57:48 +0300

    add interfaces

---
+++ ShekilPhotoGallery.Core/Interfaces/ICloudinaryService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace ShekilPhotoGallery.Core.Interfaces
+{
+    public interface ICloudinaryService
+    {
+        Task<CloudinaryUploadResult> UploadPhotoasync(IFormFile file);
+        Task<bool> DeletePhotoAsync(string publicId);
+        string GetPhotoAsync(string publicId);
+    }
+
+    public class  CloudinaryUploadResult
+    {
+        public string PublicId { get; set; } = string.Empty;
+        public string url { get; set; } = string.Empty;
+        public string SecureUrl { get; set; } = string.Empty;
+        public string Format { get; set; } = string.Empty;
+        public long FileSize { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string ThumbnailUrl { get; set; } = string.Empty;
+
+    }
+}
---
+++ ShekilPhotoGallery.Core/Interfaces/IPhotoRepository.cs
@@ -0,0 +1,20 @@
+﻿using ShekilPhotoGallery.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShekilPhotoGallery.Core.Interfaces
+{
+    public interface IPhotoRepository
+    {
+        Task<Photo> AddAsync(Photo photo);
+        Task<Photo?> GetByIdAsync(int id);
+        Task<Photo?> GetByPublicIdAsync(string publicId);
+        Task<IEnumerable<Photo>> GetAllAsync();
+        Task<IEnumerable<Photo>> GetActivePhotosAsync();
+        Task<bool> DeleteAsync(int id);
+        Task<bool> UpdateAsync(Photo photo);
+    }
+}
